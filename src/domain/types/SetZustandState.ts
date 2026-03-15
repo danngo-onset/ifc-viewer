481269dfa1099b472c9db8caa77dccd2ed0273c3
Dan Ngo <danngo.au@gmail.com>
2026-03-15 16:08:39 +1100

    Documentation

--- src/domain/types/SetZustandState.ts
+++ src/domain/types/SetZustandState.ts
@@ -1,3 +1,4 @@
 import type { StoreApi } from "zustand";
 
+/** Alias type for the internal set state function when creating a Zustand store */
 export type SetZustandState<T> = StoreApi<T>["setState"];
--- src/store/useUiStore.ts
+++ src/store/useUiStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+
 import type { SetZustandState } from "@/domain/types";
 
 type State = {
