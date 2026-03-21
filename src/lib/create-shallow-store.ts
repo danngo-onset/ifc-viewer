70b100567f048bacda991d37ab08232b97015a36
Dan Ngo <danngo.au@gmail.com>
2026-03-22 10:52:14 +1100

    Update create-shallow-store.ts

--- src/lib/create-shallow-store.ts
+++ src/lib/create-shallow-store.ts
@@ -1,6 +1,11 @@
 import { UseBoundStore, StoreApi } from "zustand";
 import { useShallow } from "zustand/shallow";
 
+/** 
+ * Utility function to create a shallow store
+ * 
+ * Allowing components to shallowly subscribe to the store without importing useShallow() from zustand
+ */
 export function createShallowStore<TStore>(useStore: UseBoundStore<StoreApi<TStore>>) {
   return function useShallowSelector<TSelector>(selector: (s: TStore) => TSelector) {
     return useStore(useShallow(selector));
