c323eb3071b97d67ae9492a674111b9470898558
Dan Ngo <danngo.au@gmail.com>
2026-04-17 21:59:34 +1000

    Update useBimComponent.ts

--- src/hooks/bim/useBimComponent.ts
+++ src/hooks/bim/useBimComponent.ts
@@ -1,21 +1,22 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useReducer, useState } from "react";
 
 import { serviceLocator } from "@/lib";
 
 import type { BimComponent } from "@/domain/enums/bim/BimComponent";
 
-type TValue<TKey extends BimComponent> = NonNullable<
+type Resolved<TKey extends BimComponent> = NonNullable<
   ReturnType<typeof serviceLocator.resolve<TKey>>
 >;
 
 export function useBimComponent<TKey extends BimComponent>(key: TKey) {
   //const [component, setComponent] = useState<BimComponentTypeMap[K] | null>(null);
-  const [component, setComponent] = useState<TValue<TKey>>();
+  const [component, setComponent] = useState<Resolved<TKey>>();
 
-  // Used to trigger re-render
-  const [, bump] = useState(0);
+  const [, triggerRerender] = useReducer(x => x + 1, 0); 
 
   useEffect(() => {
+    if (component) return;
+      
     // Poll until component is available in container
     const interval = setInterval(() => {
       const instance = serviceLocator.resolve(key);
@@ -26,14 +27,14 @@ export function useBimComponent<TKey extends BimComponent>(key: TKey) {
     }, 100);
 
     return () => clearInterval(interval);
-  }, [key]);
+  }, [key, component]);
 
   const mutateComponent = useCallback(
-    (mutate: (x: TValue<TKey>) => void) => {
+    (mutate: (x: Resolved<TKey>) => void) => {
       if (!component) return false;
 
       mutate(component);
-      bump(v => v + 1);
+      triggerRerender();
       
       return true;
     }, 
